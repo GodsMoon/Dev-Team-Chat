97816da8cdc5be73010eba4dc8d4a71772336f5f
David Shellabarger <godsmoon@gmail.com>
2010-06-30 17:39:35 -0400

    added pidgin log watcher

---
+++ watch.py
@@ -0,0 +1,71 @@
+import os
+
+import win32file
+import win32con
+
+ACTIONS = {
+  1 : "Created",
+  2 : "Deleted",
+  3 : "Updated",
+  4 : "Renamed from something",
+  5 : "Renamed to something"
+}
+# Thanks to Claudio Grondi for the correct set of numbers
+FILE_LIST_DIRECTORY = 0x0001
+
+path_to_watch = "."
+hDir = win32file.CreateFile (
+  path_to_watch,
+  FILE_LIST_DIRECTORY,
+  win32con.FILE_SHARE_READ | win32con.FILE_SHARE_WRITE,
+  None,
+  win32con.OPEN_EXISTING,
+  win32con.FILE_FLAG_BACKUP_SEMANTICS,
+  None
+)
+while 1:
+  #
+  # ReadDirectoryChangesW takes a previously-created
+  #  handle to a directory, a buffer size for results,
+  #  a flag to indicate whether to watch subtrees and
+  #  a filter of what changes to notify.
+  #
+  # NB Tim Juchcinski reports that he needed to up
+  #  the buffer size to be sure of picking up all
+  #  events when a large number of files were
+  #  deleted at once.
+  #
+  results = win32file.ReadDirectoryChangesW (
+    hDir,
+    1024,
+    True,
+    win32con.FILE_NOTIFY_CHANGE_FILE_NAME |
+     win32con.FILE_NOTIFY_CHANGE_DIR_NAME |
+     win32con.FILE_NOTIFY_CHANGE_ATTRIBUTES |
+     win32con.FILE_NOTIFY_CHANGE_SIZE |
+     win32con.FILE_NOTIFY_CHANGE_LAST_WRITE |
+     win32con.FILE_NOTIFY_CHANGE_SECURITY,
+    None,
+    None
+  )
+  for action, file in results:
+    full_filename = os.path.join (path_to_watch, file)
+    print full_filename, ACTIONS.get (action, "Unknown")
+    
+    fileHandle = open ( full_filename,"r" )
+    lineList = fileHandle.readlines()
+    fileHandle.close()
+    #print lineList
+    #print "The last line is:"
+    #print lineList[len(lineList)-1]
+    # or simply
+    lastLine = lineList[-1]
+    #print lastLine
+    
+    anchor = lastLine.split('<a href="')
+    cleanLink = ''
+    try:
+      cleanLink = anchor[1].split('"')[0]
+      print "cleanLink " + cleanLink
+    except IndexError:
+      print "no links to see here..."
